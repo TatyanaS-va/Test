a82e5390e6b860cd5b4c91358003633d4a3e8d17
Tatyana <68644664+TatyanaS-va@users.noreply.github.com>
2024-05-04 17:23:23 +0300

    Create Program1.cs

---
+++ Program1.cs
@@ -0,0 +1,18 @@
+﻿
+string AddStr(int num)
+{
+    if ((num % 10 >= 5 && num % 10 <= 9) || (num % 100 >= 11 && num % 100 <= 14) || (num % 10 == 0))
+    {
+        return num + " компьютеров";
+    }
+    else if (num % 10 == 1)
+    {
+        return num + " компьютер";
+    }
+    else if (num % 10 >= 2 && num % 10 <= 4)
+    {
+        return num + " компьютера";
+    }
+
+    return "";
+}
